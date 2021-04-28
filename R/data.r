87c4876ed8dfc1f8e6a1bd2e4dd719c960160cb3
Vincent van Hoef <vvanhoef@hotmail.com>
2021-04-28 16:44:38 +0200

    Add plasma data

--- R/data.r
+++ R/data.r
@@ -19,3 +19,25 @@
 #' }
 #' @source Delivered by Johanna
 "plasma_metadata"
+
+#' Clinical data of all the serum samples
+#'
+#' A dataset containing the metadata and clinical data of the serum samples.
+#' All samples have been put together in one data frame and the dates have been made uniform.
+#'
+#' @format A data frame with 95 rows and 19 variables:
+#' \describe{
+#'   \item{id}{ID code; unique for each sample}
+#'   \item{sampling_date}{date of sampling}
+#'   \item{birth_date}{date of birth}
+#'   \item{age}{age of patient at sampling date}
+#'   \item{sex}{gender; 1 for male, 0 for female}
+#'   \item{Diagnosis}{diagnosis; 1 for GPA, 0 for MPA}
+#'   \item{pr3-anca}{Presence of pr3 auto-antibody}
+#'   \item{mpo-anca}{Presence of mpo auto-antibody}
+#'   \item{group}{Group membership}
+#'   \item{creatinine}{Levels of creatinine}
+#'   \item{ckd_epi}{Measure for kidney activity}
+#' }
+#' @source Delivered by Johanna
+"serum_metadata"
--- data-raw/serum_metadata.R
+++ data-raw/serum_metadata.R
@@ -1,7 +1,74 @@
 ## code to prepare `serum_metadata` dataset goes here
-library("dplyr")
-library("readxl")
 
+# Create a list of the separate "blocks" of metadata in the raw data excel file
+if (!file.exists(system.file("extdata", "210303_Vasculitis.xlsx", package = "Analysis5204"))) {
+  stop("Raw data seems to be missing from the data-raw folder!")
+}
+
+# Put original data file in data-raw
+raw <- system.file("extdata", "210303_Vasculitis.xlsx", package = "Analysis5204")
+
+active_disease <- readxl::read_excel(raw,
+                                     sheet = "Serum",
+                                     range = "A3:M30",
+                                     col_names = TRUE)
+
+healthy_controls <- readxl::read_excel(raw,
+                                       sheet = "Serum",
+                                       range = "O3:W57",
+                                       col_names = TRUE)
+
+aav_remission <- readxl::read_excel(raw,
+                                    sheet = "Serum",
+                                    range = "Y3:AH10",
+                                    col_names = TRUE)
+
+disease_controls <- readxl::read_excel(raw,
+                                       sheet = "Serum",
+                                       range = "AJ3:AT10",
+                                       col_names = TRUE)
+SLE_nephritis <- split(disease_controls, disease_controls$Disease)$SLE
+
+serumList <- list("active_disease" = active_disease,
+                   "healthy_controls" = healthy_controls,
+                   "aav_remission" = aav_remission,
+                   "SLE_nephritis" = SLE_nephritis)
+
+## Rename all columns
+# Create a lookup table for the original column names
+old_colnames <- unique(unlist(sapply(serumList, function(x) colnames(x))))
+new_colnames <- dplyr::recode(old_colnames,
+                              `Date of sampling` = "sampling_date",
+                              `Date of birth` = "birth_date",
+                              `Age at sampling` = "age",
+                              `Sex (1=male, 0=female, )` = "sex",
+                              `Diagnosis (1=GPA, 0=MPA)` = "diagnosis",
+                              `...13` = "cortisone",
+                              `...11` = "das28",
+                              `Year of sampling` = "sampling_date",
+                              `Year of birth` = "birth_date",
+                              `Matched for case:` = "match_case",
+                              `Matched set number` = "match_set") %>% tolower()
+names(new_colnames) <- old_colnames
+# Rename
+serumList <- lapply(serumList, function(x) {colnames(x) <- new_colnames[colnames(x)]; x})
+# Add group name
+serumList <- lapply(names(serumList), function(x) {serumList[[x]]$group <- x; serumList[[x]]})
+
+# Merge list dfs into one df; first change dates to characters
+serumList <- lapply(serumList, function(x) {x$sampling_date <- as.character(x$sampling_date); x})
+serumList <- lapply(serumList, function(x) {x$birth_date <- as.character(x$birth_date); x})
+serum_metadata <- data.table::rbindlist(serumList, fill=TRUE)
+
+# Fix times; conversions to numeric and back to character are necessary
+serum_metadata$sampling_date[grep("^[0-9]{5}$", serum_metadata$sampling_date)] <- as.character(lubridate::as_date(as.numeric(serum_metadata$sampling_date[grep("^[0-9]{5}$", serum_metadata$sampling_date)]), origin = "1899-12-30"))
+serum_metadata$sampling_date[grep("^[0-9]{4}$", serum_metadata$sampling_date)] <- as.character(lubridate::ymd(as.numeric(serum_metadata$sampling_date[grep("^[0-9]{4}$", serum_metadata$sampling_date)]), truncated = 2L))
+serum_metadata$sampling_date <- lubridate::parse_date_time(serum_metadata$sampling_date, orders = c("ymd"))
+
+serum_metadata$birth_date[grep("^[0-9]{5}$", serum_metadata$birth_date)] <- as.character(lubridate::as_date(as.numeric(serum_metadata$birth_date[grep("^[0-9]{5}$", serum_metadata$birth_date)]), origin = "1899-12-30"))
+serum_metadata$birth_date[grep("^[0-9]{4}$", serum_metadata$birth_date)] <- as.character(lubridate::ymd(as.numeric(serum_metadata$birth_date[grep("^[0-9]{4}$", serum_metadata$birth_date)]), truncated = 2L))
+serum_metadata$birth_date <- lubridate::parse_date_time(serum_metadata$birth_date, orders = c("ymd", "y"))
 
 
 usethis::use_data(serum_metadata, overwrite = TRUE)
+
