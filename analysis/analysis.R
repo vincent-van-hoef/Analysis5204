2ac5278165ae7671213977052e6d183834e8ce34
Vincent van Hoef <vvanhoef@hotmail.com>
2021-04-30 12:34:10 +0200

    update analysis

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,7 +1,7 @@
 library("Analysis5204")
 library("ggplot2")
 
-# Raw data files are loaded automaticaly when loading the Analysis5204 package.
+# Raw data files are loaded automatically when loading the Analysis5204 package.
 
 # Create a Results directory in the top directory
 plasma_qc_dir <- "Results/Plasma/QC/"
