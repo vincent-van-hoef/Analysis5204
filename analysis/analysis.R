fb3ae290466cee9d14197b326c888284caf2464f
Vincent van Hoef <vvanhoef@hotmail.com>
2021-05-03 14:04:33 +0200

    analysis in progress

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -3,7 +3,9 @@ library("ggplot2")
 
 # Raw data files are loaded automatically when loading the Analysis5204 package.
 
-# Create a Results directory in the top directory
+# First analyse plasma only
+
+# Create a Results directory in the top directory 
 plasma_qc_dir <- "Results/Plasma/QC/"
 dir.create(plasma_qc_dir, recursive = TRUE)
 
@@ -12,3 +14,17 @@ olink_dist(plasma_npx, "Olink INFLAMMATION", paste0(plasma_qc_dir, "plasma_INF_d
 
 plasma_qc <- OlinkAnalyze::olink_qc_plot(plasma_npx)
 ggsave(paste0(plasma_qc_dir, "plasma_qc.png"), plot = plasma_qc)
+
+# Three proteins (MCP-1, OPG and uPA) are measured in both the Cardiovascular and the Inflammation panel. As seen in the values correlate well between the panels so only the Inflammation data is kept for these proteins.
+doubles <- c("MCP-1", "OPG", "uPA")
+plasma_npx <- plasma_npx[!(plasma_npx$Assay %in% doubles & plasma_npx$Panel == "Olink CARDIOVASCULAR III"),]
+
+#One sample sample (VASKA637) is filtered out due to technical concerns (failed sample according to Olink QC rapport) and another one due to relatively high level of NA values (ra1622.
+plasma_npx <- plasma_npx[!plasma_npx$SampleID %in% c("vaska637", "ra1622"),]
+
+# Impute 1 NA assays - better than throwing all sample away
+plasma_npx <- plasma_npx %>%
+  group_by(Assay) %>%
+  mutate(NPX = ifelse(is.na(NPX), median(NPX, na.rm=TRUE), NPX)) %>%
+  ungroup(Assay)
+
